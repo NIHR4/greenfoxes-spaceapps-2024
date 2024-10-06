d3387087cc9adee6692bb6b11a4c7b47bd52737f
Gigid He Chen <gigidhe@gmail.com>
2024-10-06 00:33:31 -0700

    added content in about challenge

--- src/app/challenge/page.tsx
+++ src/app/challenge/page.tsx
@@ -1,12 +1,50 @@
+import Image from "next/image"
+import challengeLogo from "./space_apps_003.webp"
+import greenHouse from "./greenHouseEffectSmall.png"
+
 export default function challenge(){
     return(
-        <div className="bg-[#C0EBA6] w-full h-full grow">
-            <div className="flex-col gap-9">
-                <h1 className="text-4xl font-bold pt-4 pl-4">The Challenge</h1>
-                <div>
-                    <p>This challenge focuses on using a combination of satellite and model-based datasets to accurately map both human-caused and natural greenhouse 
-                        gas emissions. By doing so, we aim to enhance our understanding of how these emissions contribute to global warming. For this challenge, We have 
-                        chosen to explore Topic 1: Human-Caused Emissions.</p>
+        <div className="bg-[#FFFBE6] grow flex flex-row justify-center align-middle w-full pt-4 ">
+            <div className="flex flex-col w-[80%]">
+                <div className="flex flex-row px-20 gap-8 bg-[#C0EBA6] pb-4">
+                    <div className="flex flex-col gap-6">
+                        <h1 className="text-4xl font-bold pt-4">The Challenge</h1>
+                        <div className="flex">
+                            <p>This challenge focuses on using a combination of satellite and model-based datasets to accurately map both human-caused and natural greenhouse 
+                                gas emissions. By doing so, we aim to enhance our understanding of how these emissions contribute to global warming. For this challenge, We have 
+                                chosen to explore Topic 1: Human-Caused Emissions.
+                            </p>
+                        </div>
+                    </div>
+                    <Image 
+                        src={challengeLogo}
+                        alt="SpaceApps Logo"
+                        priority
+                        width={248}
+                    />
+                </div>
+                <div className="flex flex-row gap-6 px-20 pb-4">
+                <Image 
+                        src={greenHouse}
+                        alt="GreenHouse Effect Image"
+                        priority
+                        width={300}
+                        height={200}
+                    />
+                    <div className="flex flex-col gap-6">
+                        <h1 className="text-4xl font-bold pt-4">About Greenhouse Gas Emissions</h1>
+                            <div>
+                                <p>The release of greenhouse gases by humans, particularly carbon dioxide (CO2), plays a significant role in climate change by trapping heat from the Sun in the atmosphere. 
+                                    The past decade has seen the highest recorded temperatures, primarily due to the escalating levels of these gases. This warming has led to more frequent extreme weather 
+                                    events, rising sea levels, and heightened risks to food and water security across numerous areas.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-6 px-20 bg-[#C0EBA6] pb-4">
+                    <h1 className="text-4xl font-bold pt-4">About CO2</h1>
+                    <div>
+                        <p>Carbon dioxide (CO2) is an important greenhouse gas released through activities like extracting and burning fossil fuels such as coal, oil, and natural gas, wildfires, and natural events like volcanic eruptions.</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,37 +1,29 @@
 "use client";
-import logo from "./GreenFoxesLogo.png"
-import Link from "next/link"
-import Image from "next/image"
+import logo from "./GreenFoxesLogo.png";
+import Link from "next/link";
+import Image from "next/image";
 
-
-export default function Header(){
-    return (
-        <header>
-            <div className="flex flex-row justify-between items-center gap-x-10 bg-[#347928] w-full">
-                <div className="flex items-center justify-center ">
-                    <Link href="/">
-                        <Image 
-                            src={logo}
-                            alt="LogoEquipo"
-                            priority
-                            width={100}
-                            
-                        />
-                    </Link>
-                    <h1 className="text-white text-2xl">Green Foxes</h1>
-                </div>
-                <div className="items-center justify-center align-middle pr-4 text-white">
-                    <nav className="flex gap-x-4">
-                    <Link 
-                        href={"/challenge"}
-                        >About the Challenge</Link>
-                        <Link 
-                        href={"/aboutUs"}
-                        >About Us</Link>
-
-                    </nav>
-                </div>
-            </div>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="flex flex-row bg-[#347928] w-full justify-between items-center">
+      <div className="flex flex-row items-center justify-between w-full  gap-x-10 px-24">
+        <div className="flex flex-row items-center justify-center ">
+          <Link href="/" className="flex flex-row justify-center items-center">
+            <Image src={logo} alt="LogoEquipo" priority width={100} />
+            <h1 className="text-white text-2xl">Green Foxes</h1>
+          </Link>
+        </div>
+        <div className="items-center justify-center align-middle pr-4 text-white">
+          <nav className="flex gap-x-8">
+            <Link href={"/challenge"} className="text-xl">
+              About the Challenge
+            </Link>
+            <Link href={"/aboutUs"} className="text-xl">
+              About Us
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
