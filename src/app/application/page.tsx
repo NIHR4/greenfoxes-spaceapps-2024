d7b162b558690d4fd23469b9385655c61b8fdc3d
Dahir <dahirvjc@proton.me>
2024-10-06 13:51:12 -0700

    Add landing page

---
+++ src/app/application/page.tsx
@@ -0,0 +1,27 @@
+
+
+import Image from "next/image";
+import SearchOverlay from '../components/SearchOverlay';
+import { useMemo } from "react";
+import dynamic from "next/dynamic";
+import { CircularProgress } from "@mui/material";
+export default function Application() {
+  
+  const MapComponent = useMemo(() => dynamic(
+    () => import('../components/MapComponent'),
+    { 
+      loading: () => {
+        return <div className="h-full w-full flex justify-center items-center text-green-500">
+            <CircularProgress color="inherit"/>
+        </div>
+      },
+      ssr: false
+    }
+  ), [])
+  
+  return (
+    <div className="h-full w-full bg-white grow">
+      <MapComponent/>
+    </div>
+  );
+}
